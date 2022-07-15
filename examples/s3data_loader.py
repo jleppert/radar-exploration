d6e79ec000caa98d2a36ef7c50708a5e6ef9f36b
Benjamin Wilson-Langman <benjamin-wilson-langman@uiowa.edu>
2022-07-15 14:07:04 -0500

    Completed functionality
    Added ability to pull all raw files for a specific line, or all processed files by scan folder
    
    Includes helper methods for above

--- examples/s3data_loader.py
+++ examples/s3data_loader.py
@@ -22,24 +22,24 @@ class GPRDataHelper:
     awsSecretKey = ''
     bucket = 'gpr-scan-data'
 
-    def __init__(self, accessKey, secretKey):
+    def __init__(self, accessKey, secretKey): #constructor
         self.awsAccessKey = accessKey
         self.awsSecretKey = secretKey
 
-    def download(self, scanID, folder, file):
+    def download(self, scanID, folder, file): #download for internal use
 
         s3 = boto3.client('s3', aws_access_key_id = self.awsAccessKey, aws_secret_access_key = self.awsSecretKey)
         s3Dir = os.path.join(scanID, folder, file)
         localDir = os.path.join(os.getcwd(), file)
 
         if(os.path.exists(localDir) != True):
-            self.s3.download_file(self.bucket, s3Dir, localDir)
+            s3.download_file(self.bucket, s3Dir, localDir)
 
-    def getRawData(self, scanID, lineNumber, sweepNumber):
+    def getRawData(self, scanID, lineNumber, sweepNumber): #download a raw data file given the scan folder, line number, and sweep number
 
-        fileName = str(lineNumber) + '-' + str(sweepNumber) + '.hdf5'
-        self.download(scanID, 'raw', fileName)
-        return(h5.File(os.path.join(os.getcwd(), fileName), 'r'))
+        fileName = str(lineNumber) + '-' + str(sweepNumber) + '.hdf5' #create the file name using provided parameters 
+        self.download(scanID, 'raw', fileName) #call download method
+        return(h5.File(os.path.join(os.getcwd(), fileName), 'r')) #returns the downloaded file in hdf5 format
 
     def getProcessedData(self, scanID, lineNumber):
 
@@ -47,25 +47,25 @@ class GPRDataHelper:
         self.download(scanID, 'raw', fileName)
         return(h5.File(os.path.join(os.getcwd(), fileName), 'r'))
 
-    def listRaw(self, folder):
+    def listRaw(self, scanID): #lists all files in the raw folder within given scan folder
         s3 = boto3.client('s3', aws_access_key_id = self.awsAccessKey, aws_secret_access_key = self.awsSecretKey)
-        response = s3.list_objects_v2(Bucket = self.bucket, Prefix=(folder +'/raw/'))
+        response = s3.list_objects_v2(Bucket = self.bucket, Prefix=(scanID +'/raw/'))
 
         rawList = []
         tempStr = ''
         for thing in response.get('Contents', []):
             tempStr += thing['Key']
 
-            if(tempStr[-3:] == 'df5'):
-                formatStr = tempStr.replace((folder +'/raw/'), '')
+            if(tempStr[-3:] == 'df5'): #checks if file name has finished reading so we can start copying a new one
+                formatStr = tempStr.replace((scanID +'/raw/'), '')
                 rawList.append(formatStr)
                 tempStr, formatStr = '',  ''
 
         return rawList
     
-    def listProcessed(self, folder):
+    def listProcessed(self, scanID): #lists all files in the img folder within given scan folder
         s3 = boto3.client('s3', aws_access_key_id = self.awsAccessKey, aws_secret_access_key = self.awsSecretKey)
-        response = s3.list_objects_v2(Bucket = self.bucket, Prefix=(folder +'/img/'))
+        response = s3.list_objects_v2(Bucket = self.bucket, Prefix=(scanID +'/img/'))
 
         imgList = []
         tempStr = ''
@@ -74,31 +74,66 @@ class GPRDataHelper:
             tempStr += thing['Key']
 
             if(tempStr[-3:] == 'df5' or tempStr[-3:] == 'png'):
-                formatStr = tempStr.replace((folder +'/img/'), '')
+                formatStr = tempStr.replace((scanID +'/img/'), '')
                 imgList.append(formatStr)
                 tempStr, formatStr = '',  ''
 
         return imgList
 
-    def findRaw(self, folder):
-        regex = r"(\d+)-(\d+).hdf5"
+    def findRaw(self, scanID, lineNumber): #helper function to find find all matching raw files in given scan folder
+        regex = str(lineNumber)+"-(\d+).hdf5"
 
-        listR = self.listRaw(folder)
+        rawList = []
+
+        listR = self.listRaw(scanID)
         test_str = ""
 
-        for item in listR:
+        for item in listR: #reformats list from listRaw() to something regex can use
             test_str += item + '\n'
 
         matches = re.finditer(regex, test_str, re.MULTILINE)
 
         for matchNum, match in enumerate(matches, start=1):
-            print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
-        for groupNum in range(0, len(match.groups())):
-            groupNum = groupNum + 1
-            print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+            rawList.append(match.group())
         
+        return rawList #returns list of matching file names
 
 
-    #def getRawDataByLine(self, scanId, lineNumber):
-    
+    def findProcessed(self, scanID): #helper function to find find all matching processed files in given scan folder
+        regex = r"(\d)+-bg.hdf5"
+
+        procList = []
+
+        listP = self.listProcessed(scanID)
+        test_str = ""
+
+        for item in listP: #reformats list from listRaw() to something regex can use
+            test_str += item + '\n'
+
+        matches = re.finditer(regex, test_str, re.MULTILINE)
+
+        for matchNum, match in enumerate(matches, start=1):
+            procList.append(match.group())
+        
+        return procList #returns list of matching file names
+
+    def getRawDataByLine(self, scanID, lineNumber): #get all raw files that match the line number in the given scan folder
+                                                    #and return them as a list of htf5 datasets
+        rawList = self.findRaw(scanID, lineNumber)
+        data_set = []
+        
+        for item in rawList:
+            self.download(scanID, 'raw', item) #call download method
+            data_set.append(h5.File(os.path.join(os.getcwd(), item), 'r')) #addes downloaded file to data_set list in htf5 format
+        return data_set
+
+    def getProcessedDataAll(self, scanID): #get all processed files in the given scan folder
+                                                    #and return them as a list of htf5 datasets
+        procList = self.findProcessed(scanID)
+        data_set = []
+        
+        for item in procList:
+            self.download(scanID, 'img', item) #call download method
+            data_set.append(h5.File(os.path.join(os.getcwd(), item), 'r')) #addes downloaded file to data_set list in htf5 format
+        return data_set
+
